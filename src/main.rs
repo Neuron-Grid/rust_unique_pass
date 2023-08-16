2c65fdc29f53fc1bf7ed1b2bcc7e842108c5c237
Neuron Grid <neuron-grid@neuron-grid.net>
2023-08-16 19:47:10 +0900

    エラー修正をしました。 エラーハンドリングを追加しました。 可読性を上げました。 コードの重複をなくしました。 コメントでの説明を追加しました。

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,18 @@ extern crate zxcvbn;
 use rand::{rngs::OsRng, seq::SliceRandom};
 use std::io;
 
+// ユーザからの入力に基づいて強固なパスワードを生成し、表示します。
 fn main() {
-    // パスワードの長さの取得
+    // ユーザーから指定されたパスワードの長さを格納します。
     let length = get_password_length();
-
-    // 文字のセットを取得
-    let character_set = get_character_set();
-
-    // 強力なパスワードを生成する
-    let mut password = generate_password(&character_set, length);
-    while !is_strong(&password) {
-        password = generate_password(&character_set, length);
-    }
-
+    // ユーザーの選択に基づいて組み立てられた文字セットを格納します。
+    let character_set = assemble_character_set();
+    // 生成された強力なパスワードを格納します。
+    let password = produce_secure_password(&character_set, length);
     println!("生成されたパスワード: {}", password);
 }
 
+// ユーザーからの入力を取得し、トリムして返します。
 fn get_input(prompt: &str) -> String {
     let mut input = String::new();
     println!("{}", prompt);
@@ -32,18 +28,20 @@ enum PasswordLengthError {
     TooShort,
 }
 
+// パスワードの長さを検証する
 fn validate_password_length(input: &str) -> Result<usize, PasswordLengthError> {
+    // 文字列をisize型に変換して解析します。isize型を使用して負の数も考慮します。
     match input.parse::<isize>() {
-        // isize型に変更して、負の数も解析できるようにします。
-        Ok(n) if n <= 0 => Err(PasswordLengthError::InvalidNumber), // 0以下の場合もエラーハンドリング
+        Ok(n) if n <= 0 => Err(PasswordLengthError::InvalidNumber),
         Ok(n) if n as usize >= 8 => Ok(n as usize),
         Ok(_) => Err(PasswordLengthError::TooShort),
         Err(_) => Err(PasswordLengthError::InvalidNumber),
     }
 }
 
+// パスワードの長さを取得します。
 fn get_password_length() -> usize {
-    println!("パスワードの長さを入力してください\n12以上を推奨します。");
+    println!("パスワードの長さを入力してください\n12文字以上を推奨します。");
     loop {
         let input = get_input("");
         match validate_password_length(&input) {
@@ -52,27 +50,28 @@ fn get_password_length() -> usize {
                 println!("有効な数値を入力してください。");
                 continue;
             }
-            Err(PasswordLengthError::TooShort) => println!("推奨される長さは12以上です。"),
+            Err(PasswordLengthError::TooShort) => println!("推奨される長さは12文字以上です。"),
         }
     }
 }
 
-fn get_character_set() -> String {
+// ユーザーの選択に基づいて文字セットを組み立てて返します。
+fn assemble_character_set() -> String {
+    // 各質問とそれに対応する文字セットをペアとして保持します。
+    let questions = [
+        ("大文字を含めますか？ (y/n)", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+        ("小文字を含めますか？ (y/n)", "abcdefghijklmnopqrstuvwxyz"),
+        ("数字を含めますか？ (y/n)", "0123456789"),
+    ];
+    // デフォルトで使用される特殊文字のセットを定義します。
     let default_special_chars = "!@#$%^&*()-_=+';:,.<>/?";
-    let mut character_set: String = String::new();
-
-    if ask_user("大文字を含めますか？ (y/n)") {
-        character_set += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // 選択に基づいて組み立てられる文字セットを一時的に格納します。
+    let mut assembled_charset: String = String::new();
+    for (question, chars) in questions.iter() {
+        if ask_user(question) {
+            assembled_charset += chars;
+        }
     }
-
-    if ask_user("小文字を含めますか？ (y/n)") {
-        character_set += "abcdefghijklmnopqrstuvwxyz";
-    }
-
-    if ask_user("数字を含めますか？ (y/n)") {
-        character_set += "0123456789";
-    }
-
     println!(
         "デフォルトで使用される特殊文字は{}です。",
         default_special_chars
@@ -82,24 +81,18 @@ fn get_character_set() -> String {
             if ask_user("使用する特殊文字を変更しますか？ (y/n)") {
                 let special_chars_input =
                     get_input("使用する特殊文字を入力してください (例: !@#):");
-                if special_chars_input
-                    .chars()
-                    .all(|c| default_special_chars.contains(c))
-                {
-                    character_set += &special_chars_input;
-                    break;
-                } else {
-                    println!("無効な特殊文字が入力されました。再度入力してください。");
-                }
+                assembled_charset += &special_chars_input;
+                break;
             } else {
-                character_set += default_special_chars;
+                assembled_charset += default_special_chars;
                 break;
             }
         }
     }
-    character_set
+    assembled_charset
 }
 
+// ユーザーに質問する
 fn ask_user(message: &str) -> bool {
     loop {
         let input = get_input(message);
@@ -111,14 +104,32 @@ fn ask_user(message: &str) -> bool {
     }
 }
 
-fn generate_password(chars: &str, length: usize) -> String {
+// 指定された文字セットと長さに基づいて、強力なパスワードを生成します
+fn produce_secure_password(chars: &str, length: usize) -> String {
+    let mut password;
+    loop {
+        password = assemble_random_password(chars, length);
+        if is_strong(&password) {
+            break;
+        }
+    }
+    password
+}
+
+// 指定された文字セットと長さに基づいてランダムなパスワードを組み立てます。
+fn assemble_random_password(chars: &str, length: usize) -> String {
+    // セキュアな乱数生成のための乱数生成器を初期化します。
     let mut rng = OsRng;
+    // 文字列を char のベクタに変換します。
+    let chars_vec: Vec<char> = chars.chars().collect();
     (0..length)
-        .map(|_| chars.as_bytes().choose(&mut rng).unwrap().clone() as char)
+        .map(|_| *chars_vec.choose(&mut rng).unwrap())
         .collect()
 }
 
+// パスワードの強度をチェックする
 fn is_strong(password: &str) -> bool {
+    // zxcvbnライブラリを使用して、パスワードの強度を評価します。
     let result = zxcvbn::zxcvbn(password, &[]).unwrap();
     result.score() > 3
 }
