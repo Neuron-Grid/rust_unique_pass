97f9b0d757312c92db4e88834921f48e2320d741
Neuron Grid <neuron-grid@neuron-grid.net>
2023-08-25 03:37:49 +0900

    可読性を確保する為、main.rsとi18n.rsに分離しました。 mainはパスワードの生成と表示をします。 i18nは多言語処理をします。(現在テスト中)

---
+++ src/i18n.rs
@@ -0,0 +1,100 @@
+/*
+Copyright 2023 Neuron Grid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+use fluent::{FluentBundle, FluentResource};
+use std::{fs, str::FromStr};
+use unic_langid::LanguageIdentifier;
+
+// デフォルトの言語英語に定義します。
+const DEFAULT_LANGUAGE: &str = "en-US";
+
+pub fn map_to_fluent_code(code: &str) -> LanguageIdentifier {
+    match LanguageIdentifier::from_str(code) {
+        Ok(lang_id) => lang_id,
+        Err(_) => {
+            // デフォルトのエラーメッセージを表示する
+            eprintln!("Failed to parse the provided language identifier.");
+            std::process::exit(1);
+        }
+    }
+}
+
+// ユーザーの入力からパスワードの長さを決定します。
+pub fn initialize_bundle(matches: &clap::ArgMatches) -> FluentBundle<FluentResource> {
+    let language = matches.value_of("language").unwrap_or(DEFAULT_LANGUAGE);
+    match load_fluent_bundle(language) {
+        Some(bundle) => bundle,
+        None => {
+            eprintln!("翻訳バンドルのロードに失敗しました。");
+            std::process::exit(1);
+        }
+    }
+}
+
+// 指定された言語のFTLファイルを読み込み、Fluentバンドルを返します。
+pub fn load_fluent_bundle(language: &str) -> Option<FluentBundle<FluentResource>> {
+    let fluent_code = map_to_fluent_code(language);
+    let ftl_filepath = format!("./translation/{}.ftl", fluent_code);
+    // 指定された言語のFTLファイルが存在するかどうかを確認
+    if !std::path::Path::new(&ftl_filepath).exists() {
+        eprintln!("エラー: {}\nファイルが存在しません。", ftl_filepath);
+        return None;
+    }
+    let ftl_string = match fs::read_to_string(&ftl_filepath) {
+        Ok(content) => content,
+        Err(_) => {
+            eprintln!("FTLファイルを読み取れません。\nFTL file reading not possible.");
+            std::process::exit(1);
+        }
+    };
+    let ftl_resource = match FluentResource::try_new(ftl_string) {
+        Ok(resource) => resource,
+        Err(_) => {
+            eprintln!("FTL文字列をパースできませんでした。\nFTL string could not be parsed.");
+            std::process::exit(1);
+        } /*
+            これはテスト用のコードです。
+            This is a test code.
+            Err(error) => {
+                eprintln!("FTL文字列をパースできませんでした。\n{:?}", error);
+                std::process::exit(1);
+            }
+          */
+    };
+    let langid = fluent_code;
+    let mut bundle = FluentBundle::new(vec![&langid]);
+    bundle
+        .add_resource(ftl_resource)
+        .expect("FTLリソースの追加に失敗しました。\nFTL resource could not be added.");
+    Some(bundle)
+}
+
+pub fn get_translation(
+    bundle: &FluentBundle<FluentResource>,
+    key: &str,
+    args: Option<&fluent::FluentArgs>,
+) -> String {
+    if let Some(message) = bundle.get_message(key) {
+        let value = match &message.value {
+            Some(v) => v,
+            None => return "翻訳が見つかりません。\nTranslation not found.".to_string(),
+        };
+        let result = bundle.format_pattern(value, args, &mut vec![]);
+        result.to_string()
+    } else {
+        "翻訳が見つかりません。\nTranslation not found.".to_string()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
-// Copyright 2023 Neuron Grid
+/*
+Copyright 2023 Neuron Grid
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-//     https://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
+mod i18n;
 extern crate rand;
 extern crate zxcvbn;
-
 use clap::{App, Arg};
 use fluent::{FluentBundle, FluentResource};
+use i18n::{get_translation, initialize_bundle};
 use rand::{rngs::OsRng, seq::SliceRandom};
-use std::{fs, io, str::FromStr};
-use unic_langid::LanguageIdentifier;
-
-// デフォルトの言語英語に定義します。
-const DEFAULT_LANGUAGE: &str = "en-US";
+use std::io;
 
 // ユーザからの入力に基づいて強固なパスワードを生成し、表示します。
 fn main() {
@@ -76,7 +75,7 @@ fn get_password_length(bundle: &FluentBundle<FluentResource>) -> usize {
         // get_inputにbundleを渡します
         let input = get_input("", bundle);
         match validate_password_length(&input) {
-            Ok(n) => return n,
+            Ok(definitely) => return definitely,
             Err(PasswordLengthError::InvalidNumber) => {
                 // 翻訳キーのerror_invalid_numberを表示させる。
                 println!("{}", get_translation(bundle, "error_invalid_number", None));
@@ -158,10 +157,12 @@ fn ask_user(message: &str, bundle: &FluentBundle<FluentResource>) -> bool {
     loop {
         let input = get_input(message, bundle);
         match input.to_lowercase().as_str() {
+            // english
             "y" => return true,
             "n" => return false,
             "yes" => return true,
             "no" => return false,
+            // japanese
             "はい" => return true,
             "いいえ" => return false,
             // 翻訳キーのerror_invalid_inputを表示させる。
@@ -202,92 +203,25 @@ fn is_strong(password: &str) -> bool {
 
 // ユーザーが指定した言語に基づいて、Fluentファイルの名前を返します。
 fn parse_args() -> clap::ArgMatches {
-    App::new("passgen")
-        .version("0.2 beta")
+    App::new("rupass")
+        .version("0.3 beta")
         .author("Neuron Grid <neuron-grid@neurongrid.net>")
-        .about("Generate strong passwords.")
+        .about("rust unique pass: Generate strong password.")
         .arg(
             Arg::with_name("language")
                 .short('l')
                 .long("language")
                 .value_name("LANGUAGE")
-                .help("Sets a specific language for messages (e.g., jp, en)")
+                .help("Sets the language for user prompts and messages. \nSupported Language: ja and en.")
                 .takes_value(true),
         )
+        /*.arg(
+            Arg::with_name("number")
+                .short('n')
+                .long("number")
+                .value_name("NUMBER")
+                .help("Sets the number of passwords to generate.")
+                .takes_value(true),
+        )*/
         .get_matches()
-}
-
-fn map_to_fluent_code(code: &str) -> LanguageIdentifier {
-    match LanguageIdentifier::from_str(code) {
-        Ok(lang_id) => lang_id,
-        Err(_) => {
-            // Display a default error message
-            eprintln!("Failed to parse the provided language identifier.");
-            std::process::exit(1);
-        }
-    }
-}
-
-fn initialize_bundle(matches: &clap::ArgMatches) -> FluentBundle<FluentResource> {
-    let language = matches.value_of("language").unwrap_or(DEFAULT_LANGUAGE);
-    match load_fluent_bundle(language) {
-        Some(bundle) => bundle,
-        None => {
-            eprintln!("翻訳バンドルのロードに失敗しました。");
-            std::process::exit(1);
-        }
-    }
-}
-
-// 指定された言語のFTLファイルを読み込み、Fluentバンドルを返します。
-fn load_fluent_bundle(language: &str) -> Option<FluentBundle<FluentResource>> {
-    let fluent_code = map_to_fluent_code(language);
-    let ftl_filepath = format!("./translation/{}.ftl", fluent_code);
-    // 指定された言語のFTLファイルが存在するかどうかを確認
-    if !std::path::Path::new(&ftl_filepath).exists() {
-        eprintln!("エラー: {}\nファイルが存在しません。", ftl_filepath);
-        return None;
-    }
-    let ftl_string = match fs::read_to_string(&ftl_filepath) {
-        Ok(content) => content,
-        Err(_) => {
-            eprintln!("FTLファイルを読み取れません。\nFTL file reading not possible.");
-            std::process::exit(1);
-        }
-    };
-    let ftl_resource = match FluentResource::try_new(ftl_string) {
-        Ok(resource) => resource,
-        Err(_) => {
-            eprintln!("FTL文字列をパースできませんでした。\nFTL string could not be parsed.");
-            std::process::exit(1);
-        } // これはテスト用のコードです。
-          // This is a test code.
-          // Err(e) => {
-          //     eprintln!("FTL文字列をパースできませんでした。\n{:?}", e);
-          //     std::process::exit(1);
-          // }
-    };
-    let langid = fluent_code;
-    let mut bundle = FluentBundle::new(vec![&langid]);
-    bundle
-        .add_resource(ftl_resource)
-        .expect("FTLリソースの追加に失敗しました。\nFTL resource could not be added.");
-    Some(bundle)
-}
-
-fn get_translation(
-    bundle: &FluentBundle<FluentResource>,
-    key: &str,
-    args: Option<&fluent::FluentArgs>,
-) -> String {
-    if let Some(message) = bundle.get_message(key) {
-        let value = match &message.value {
-            Some(v) => v,
-            None => return "翻訳が見つかりません。\nTranslation not found.".to_string(),
-        };
-        let result = bundle.format_pattern(value, args, &mut vec![]);
-        result.to_string()
-    } else {
-        "翻訳が見つかりません。\nTranslation not found.".to_string()
-    }
 }
